7a8807888d0253e69b95f2bac168d43c400d8741
josepenasaa-create <jose.penasaa@cun.edu.co>
2026-05-05 18:08:20 -0500

    Update geminiService.ts

--- techbridge---plataforma-de-carrera-para-ingeniería/src/services/geminiService.ts
+++ techbridge---plataforma-de-carrera-para-ingeniería/src/services/geminiService.ts
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "https://esm.run";
 import { ChatMessage } from "../types";
 
 const genAI = new GoogleGenerativeAI("AIzaSyDk4cvF2Nq0beFP_6nezmMF7rjva5GU1GI");
